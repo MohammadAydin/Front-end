51d1fa641a2ec4ce5329d65d42658807e8de5803
yahiaalsetam <yahiaalsatam2024@gmail.com>
2025-07-20 00:13:42 +0300

    edit download app section landing page

--- landingPage/src/components/Hero.jsx
+++ landingPage/src/components/Hero.jsx
@@ -14,10 +14,11 @@ const Hero = () => {
           eines starken Netzwerks – einfach registrieren und loslegen!
         </h2>
         <div className="HeroActions">
-          <button className="DownloadBtn">
-            <p className="Warning">Unsere App kommt bald</p>
-            <HiDownload /> Herunterladen
-          </button>
+          <a href="https://play.google.com/store/apps/details?id=com.w.wowann">
+            <button className="DownloadBtn">
+              <HiDownload /> Download App
+            </button>
+          </a>
           <button className="LearnMoreBtn">
             <HiOutlineSearch /> Mehr Erfahrung
           </button>
