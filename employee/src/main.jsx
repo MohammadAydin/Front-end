c8c2272d360383e91b245c27ddbc72e30594d105
yahiaalsetam <yahiaalsatam2024@gmail.com>
2025-08-28 19:05:31 +0300

    edit main

--- employee/src/main.jsx
+++ employee/src/main.jsx
@@ -1,23 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { LoadScript } from "@react-google-maps/api";
-
-import App from "./App";
 import "./index.css";
+import App from "./App";
 import "./responsive.css";
-import "./i18n";
+import { LoadScript } from "@react-google-maps/api";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { toast } from "react-toastify";
+import './i18n';
 
 const queryClient = new QueryClient();
 
+
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+    <LoadScript googleMapsApiKey="AIzaSyDH-rfDKqld3jf64z84P9e34iNBkdSwZlw">
+      <QueryClientProvider client={queryClient}>
         <App />
-      </LoadScript>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </LoadScript>
   </StrictMode>
 );
--- employer/src/main.jsx
+++ employer/src/main.jsx
@@ -3,26 +3,22 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { LoadScript } from "@react-google-maps/api";
-import { useAuthStore } from "./store/useAuthStore";
 
 import "./index.css";
 import "./responsive.css";
-import "./i18n";
 import App from "./App.jsx";
+import "./i18n";
 
-if (import.meta.env.DEV) {
-  window.useAuthStore = useAuthStore;
-}
-
+// create a new instance of QueryClient
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+    <LoadScript googleMapsApiKey="AIzaSyDH-rfDKqld3jf64z84P9e34iNBkdSwZlw">
+      <QueryClientProvider client={queryClient}>
         <App />
-      </LoadScript>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </LoadScript>
   </StrictMode>
 );
